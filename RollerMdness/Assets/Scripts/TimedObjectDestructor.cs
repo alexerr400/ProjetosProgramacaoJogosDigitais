19f17bef7cd0d2d1ead8bfc0891f7160cf363a51
ALEXANDRE RICOY LEITE LOPES <u15157@academico.cotuca.unicamp.br>
2017-11-10 11:12:08 -0200

    Roller Madness adicionado

---
+++ RollerMdness/Assets/Scripts/TimedObjectDestructor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedObjectDestructor : MonoBehaviour {
+
+	public float timeOut = 1.0f;
+	public bool detachChildren = false;
+    public GameObject explosionPrefab;
+
+    // Use this for initialization
+    void Awake () {
+		// invote the DestroyNow funtion to run after timeOut seconds
+		Invoke ("DestroyNow", timeOut);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void DestroyNow ()
+	{
+        if (explosionPrefab != null)
+        {
+            Instantiate(explosionPrefab, transform.position, transform.rotation);
+        }
+        if (detachChildren) { // detach the children before destroying if specified
+			transform.DetachChildren ();
+		}
+		DestroyObject (gameObject);
+	}
+}
